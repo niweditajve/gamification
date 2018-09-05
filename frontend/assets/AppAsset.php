0542d2450880cce21f71bd50895b4001330446c6
Akanksha Maurya <akankshamaurya@virtual.local>
2018-09-05 18:32:40 +0530

    changes made for forntend dashboard

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,8 +13,12 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/barGraphs.css',
     ];
     public $js = [
+        'js/barGraphs.js',
+        'js/jquery.circlechart.js',
+        'js/circleChart.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
---
+++ frontend/web/js/circleChart.js
@@ -0,0 +1,34 @@
+
+$('.demo-1').percentcircle({
+	animate : true,
+diameter : 100,
+guage: 3,
+coverBg: '#fff',
+bgColor: '#efefef',
+fillColor: '#E95546',
+percentSize: '15px',
+percentWeight: 'normal'
+});
+
+$('.demo-2').percentcircle({
+animate : true,
+diameter : 100,
+guage: 3,
+coverBg: '#fff',
+bgColor: '#efefef',
+fillColor: 'yellow',
+percentSize: '15px',
+percentWeight: 'normal'
+});
+
+$('.demo-3').percentcircle({
+animate : true,
+diameter : 100,
+guage: 3,
+coverBg: '#fff',
+bgColor: '#efefef',
+fillColor: 'green',
+percentSize: '15px',
+percentWeight: 'normal'
+});
+
